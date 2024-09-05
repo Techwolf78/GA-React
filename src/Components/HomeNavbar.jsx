a96bf10da81b18a2dfe14a66e73abfc3f3b60a9c
eparis@atlassian.com <gryphonvideos@Gryphons-MacBook-Pro.local>
2024-09-05 13:09:33 +0530

    cdsd

--- src/Components/HomeNavbar.jsx
+++ src/Components/HomeNavbar.jsx
@@ -8,10 +8,11 @@ const HomeNavbar = () => {
       <div className="container mx-auto flex justify-between items-center px-4">
         <div className="text-2xl font-bold logo-animation">Gryphon Academy</div>
         <ul className="flex space-x-8">
-          <li><Link to="/" className="hover:text-gray-300 transition-all duration-300">Home</Link></li>
+     
           <li><Link to="/about" className="hover:text-gray-300 transition-all duration-300">About</Link></li>
           <li><Link to="/gax" className="hover:text-gray-300 transition-all duration-300">GAX</Link></li>
           <li><Link to="/blogs" className="hover:text-gray-300 transition-all duration-300">Blogs</Link></li>
+          <li><Link to="/contact" className="hover:text-gray-300 transition-all duration-300">Contact Us</Link></li>
         </ul>
       </div>
     </nav>
