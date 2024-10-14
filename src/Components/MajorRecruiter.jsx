24df6069b1ffaee6b7834325e589a4573c0bdfc2
techwolf78 <mrajaypawar207@gmail.com>
2024-10-14 19:01:21 +0530

    wfukd push

--- src/Components/MajorRecruiter.jsx
+++ src/Components/MajorRecruiter.jsx
@@ -2,36 +2,49 @@ import React from 'react';
 
 const MajorRecruiter = () => {
   const logos = [...Array(12)].map((_, index) => (
-    <div key={index} className="logo-slider-item flex-none mx-2">
+    <div key={index} className="logo-slider-item flex-none mx-4 my-2">
       <img
         src={`Logos/${index + 1}.jpg`}
         alt="College Logo"
-        className="h-12 sm:h-16 md:h-20 max-w-full object-contain" // Reduced logo height
+        className="h-12 sm:h-16 md:h-20 max-w-full object-contain"
       />
     </div>
   ));
 
   return (
-    <div className="logo-slider-section bg-[#003073] py-0 px-0 roboto-regular">
-      <div className="container mx-auto">
-        <p className="section-heading text-[#FFC80E] text-center text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold mb-3 pt-1 ">
+    <div className="logo-slider-section bg-[#003073] roboto-regular">
+      <div className="logo-slider relative overflow-hidden w-full ">
+      <p className="section-heading text-[#FFC80E] text-center text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold mb-3 pt-1 ">
           OUR TOP RECRUITERS
         </p>
-        <div className="logo-slider relative overflow-hidden w-full ">
-          <div className="logo-slider-track flex animate-scroll mb-4">
-            {/* Duplicate the logos for infinite scroll effect */}
-            {[...logos, ...logos]}
-          </div>
+        <div className="logo-slider-track flex animate-scroll ">
+          {/* Duplicate the logos for infinite scroll effect */}
+          {[...logos, ...logos]}
         </div>
       </div>
 
       {/* Inline styles for custom CSS */}
       <style>
         {`
+          .logo-slider-section {
+            padding: 0; /* Remove padding */
+            margin: 0; /* Remove margin */
+          }
+
+          .logo-slider {
+            width: 100%; /* Ensure full width */
+          }
+
           .logo-slider-track {
             display: flex; /* Ensure the logos are in a row */
             width: calc(120px * 12); /* Adjust based on the reduced logo size */
-            animation: scroll 20s linear infinite;
+            animation: scroll 5s linear infinite;
+          }
+
+          @media (min-width: 768px) {
+            .logo-slider-track {
+              animation: scroll 10s linear infinite;
+            }
           }
 
           .logo-slider-track:hover {
