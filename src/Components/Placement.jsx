6792a51cd04e19caa015292294dc9d88b5ec5c5b
techwolf78 <mrajaypawar207@gmail.com>
2024-10-15 15:14:28 +0530

    we aint for t push

--- src/Components/Placement.jsx
+++ src/Components/Placement.jsx
@@ -12,7 +12,7 @@ import SuccessfulDrives from "./SuccessfulDrives";
 import MajorRecruiter from "./MajorRecruiter";
 import ConnectWithUs from "./ConnectWithUs";
 import HookLine from "./HookLine"; // Import the new component
-import PlacementProcess from "./PlacementProcess";
+import PlacementKnowMore from "./PlacementKnowMore";
 
 const Placement = () => {
   return (
@@ -27,9 +27,21 @@ const Placement = () => {
 
       <PartneredCollege />
 
-    <PlacementProcess />
+      {/* New Section: Simple Placement Process */}
+      <div className="p-0 text-center roboto-regular">
+        <p className="text-2xl sm:text-3xl lg:text-4xl xl:text-4xl font-bold text-[#FFC80E] bg-[#003073] py-4">
+          WE FOLLOW SIMPLE PLACEMENT PROCESS
+        </p>
+        <div className="w-full h-auto bg-[#003073]">
+          <img 
+            src="placement-bg/GA - placement ProcessNew.PNG" 
+            alt="Infographic" 
+            className="w-full h-auto block px-1 py-1"
+          />
+        </div>
+      </div>
       
-     
+     <PlacementKnowMore />
       <Advisory />
       <SuccessfulDrives />
       <RecruiterTestimonial />
