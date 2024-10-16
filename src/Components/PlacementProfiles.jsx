77ffb0c9f2be12edc0540aa32de3cd70928afaf4
techwolf78 <mrajaypawar207@gmail.com>
2024-10-16 16:25:36 +0530

    we aint for t push

--- src/Components/PlacementProfiles.jsx
+++ src/Components/PlacementProfiles.jsx
@@ -78,11 +78,7 @@ const PlacementProfiles = () => {
         </div>
       </section>
 
-      <div className="mt-8 text-center">
-        <button className="bg-yellow-300 text-gray-800 py-2 px-4 rounded hover:bg-yellow-400 transition-all duration-300">
-          Know More
-        </button>
-      </div>
+
     </div>
   );
 };
