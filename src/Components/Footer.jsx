2b8a1e4eede361845a54c0e4bd66ce6f388ac6d2
Techwolf78 <mrajaypawar207@gmail.com>
2025-12-08 17:25:14 +0530

    Update phone number to +91 8956444509 in Footer and Contact components

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -64,7 +64,7 @@ const Footer = () => {
             <li className="flex items-center justify-center md:justify-start">
               <FontAwesomeIcon icon={faMobileAlt} className="text-yellow-400 mr-2" />
               <p className="text-sm">
-                <a href="tel:+918983614509" className="hover:underline hover:text-yellow-400 transition duration-300">+91 89836 14509</a>
+                <a href="tel:+918956444509" className="hover:underline hover:text-yellow-400 transition duration-300">+91 8956444509</a>
               </p>
 
             </li>
